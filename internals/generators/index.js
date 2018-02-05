f7fe5b8ce10dce294b5ef02b0796b0a97d6883e7
Akhil P <akhil.pab@gmail.com>
2018-02-05 16:25:19 +0530

    Add route generator

--- internals/generators/index.js
+++ internals/generators/index.js
@@ -3,8 +3,10 @@
  */
 const componentGenerator = require('./component/');
 const containerGenerator = require('./container/');
+const routeGenerator = require('./route/');
 
 module.exports = (plop) => {
   plop.setGenerator('component', componentGenerator);
   plop.setGenerator('container', containerGenerator);
+  plop.setGenerator('route', routeGenerator);
 };
---
+++ internals/generators/route/index.js
@@ -0,0 +1,72 @@
+/**
+ * Route generator
+ */
+
+const fs = require('fs');
+const path = require('path');
+const containerExist = require('../../utils/containerExists');
+
+const loadableExists = (value) => {
+  const filePath = path.resolve(process.cwd(), 'client', 'containers', value, 'Loadable.js');
+  return fs.existsSync(filePath);
+};
+
+function trimTemplateFile(template) {
+  return fs.readFileSync(path.join(__dirname, `./${template}`), 'utf8');
+}
+
+function trimTemplateFileToImport(template) {
+  return fs.readFileSync(path.join(__dirname, `./${template}`), 'utf8');
+}
+
+module.exports = {
+  description: 'Add a route',
+  prompts: [{
+    type: 'input',
+    name: 'container',
+    message: 'Which container should the route show?',
+    validate: (value) => {
+      if ((/.+/).test(value)) {
+        return containerExist(value) ? true : `"${value}" Container does not exists.`;
+      }
+
+      return 'Container is required.';
+    },
+  }, {
+    type: 'input',
+    name: 'path',
+    message: 'What is the path of the route? (Do not use relative path.)',
+    validate: (value) => {
+      if ((/.+/).test(value)) {
+        return true;
+      }
+
+      return 'Container is required.';
+    },
+  }],
+
+  actions: (data) => {
+    const actions = [{
+      type: 'modify',
+      path: path.resolve(process.cwd(), 'client', 'routes.js'),
+      pattern: /\{\s*(component: NotFound,\n\s*\},)/g,
+      template: trimTemplateFile('route.hbs'),
+    }];
+
+    const routeImport = {
+      type: 'modify',
+      path: path.resolve(process.cwd(), 'client', 'routes.js'),
+      pattern: /\nexport default \[/g,
+    };
+
+    if (loadableExists(data.container)) {
+      routeImport.template = trimTemplateFile('routeImportLoadable.hbs');
+    } else {
+      routeImport.template = trimTemplateFile('routeImport.hbs');
+    }
+
+    actions.push(routeImport);
+
+    return actions;
+  },
+};
